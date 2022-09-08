bf7b7c78d3ae436b98d101ac5540b65db5493657
Abhi <guptaabhinav609@gmail.com>
2022-09-09 01:10:54 +0530

    Create StocksObservable.java

---
+++ observer_pattern/oberservable/StocksObservable.java
@@ -0,0 +1,10 @@
+import com.observerpattern.Observer.NotificationAlertObserver;
+
+public interface StocksObservable {
+
+    public void add(NotificationAlertObserver observer);
+    public void remove(NotificationAlertObserver observer);
+    public void notifySubscribers();
+    public void setStockCount(int newStockAdded);
+    public int getStockCount();
+}

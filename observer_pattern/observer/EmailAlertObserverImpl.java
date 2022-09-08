57db40376601334aa74eb3d3d1cad4ae5c23409d
Abhi <guptaabhinav609@gmail.com>
2022-09-09 01:08:01 +0530

    Create EmailAlertObserverImpl.java

---
+++ observer_pattern/observer/EmailAlertObserverImpl.java
@@ -0,0 +1,18 @@
+public class EmailAlertObserverImpl implements NotificationAlertObserver {
+    String emailId;
+    StocksObservable observable;
+
+    public EmailAlertObserverImpl(String emailId, StocksObservable observable) {
+        this.emailId = emailId;
+        this.observable = observable;
+    }
+
+    @Override
+    public void update() {
+        sendMail("product is stocked up...hurry!");
+    }
+
+    private void sendMail(String msg) {
+        System.out.println("email sent to: " + emailId);
+    }
+}

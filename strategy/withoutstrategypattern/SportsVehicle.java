cdd8f5b7fb2439c8af484f0563c9a7cd01bcc03c
Abhi <guptaabhinav609@gmail.com>
2022-09-09 01:25:56 +0530

    Create SportsVehicle.java

---
+++ strategy/withoutstrategypattern/SportsVehicle.java
@@ -0,0 +1,8 @@
+public class SportsVehicle extends Vehicle {
+
+    @Override
+    public void drive() {
+        //different drive algorithm
+        System.out.println("sports drive capability");
+    }
+}

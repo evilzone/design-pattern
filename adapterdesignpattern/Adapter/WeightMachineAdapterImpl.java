c16c3d2ad9f52fac991be52ec905f59f7009375c
Abhi <guptaabhinav609@gmail.com>
2023-04-04 13:48:53 +0530

    Update WeightMachineAdapterImpl.java

--- adapterdesignpattern/Adapter/WeightMachineAdapterImpl.java
+++ adapterdesignpattern/Adapter/WeightMachineAdapterImpl.java
@@ -3,10 +3,13 @@ package com.adapterdesignpattern.Adapter;
 import com.adapterdesignpattern.Adaptee.WeightMachine;
 
 public class WeightMachineAdapterImpl implements WeightMachineAdapter {
+    
     private WeightMachine weightMachine;
+    
     public WeightMachineAdapterImpl(WeightMachine weightMachine) {
         this.weightMachine = weightMachine;
     }
+    
     @Override
     public double getWeightInKgs() {
         return weightMachine.getWeightInPound() * 0.45;

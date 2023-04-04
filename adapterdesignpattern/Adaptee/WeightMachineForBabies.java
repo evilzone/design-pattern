c76f09707eae373bbc9fd79a54cfa98d56aee13b
Abhi <guptaabhinav609@gmail.com>
2023-04-04 13:49:29 +0530

    Update WeightMachineForBabies.java

--- adapterdesignpattern/Adaptee/WeightMachineForBabies.java
+++ adapterdesignpattern/Adaptee/WeightMachineForBabies.java
@@ -1,6 +1,7 @@
 package com.adapterdesignpattern.Adaptee;
 
 public class WeightMachineForBabies implements WeightMachine {
+    
     @Override
     public double getWeightInPound() {
         return 28;

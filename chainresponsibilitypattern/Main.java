ce35e4d4a54eba3c3a65a0a54fa808eb7aa07055
Abhi <guptaabhinav609@gmail.com>
2023-06-08 12:54:56 +0530

    Delete chainresponsibilitypattern directory

--- chainresponsibilitypattern/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-  public static void main(String[] args) {
-    LogProcessor processor =
-            new InfoLogProcessor(new DebugLogProcessor(new ErrorLogProcessor(null)));
-    processor.log(LogProcessor.ERROR, "exception happens");
-    processor.log(LogProcessor.DEBUG, "need to debug this");
-    processor.log(LogProcessor.INFO, "info happens");
-  }
-}
